dda6ed9bb6bca226f5831e385ba5a45e875d9e66
Matthew Crittle <legendsmith.au@gmail.com>
2018-09-09 01:20:36 +1000

    IT WORKS BOYS

---
+++ main.js
@@ -0,0 +1,64 @@
+function dice(max) {
+	return (Math.floor(Math.random()*(max)));
+}
+
+
+
+function dreplace(descinput, dparams) {
+	var i=-1;
+	var n=dparams.length;
+	var pattern
+	var doutput=descinput;
+	while ( ++ i < n ) {
+		pattern=new RegExp("%"+i,"g");
+		doutput=doutput.replace(pattern,dparams[i]);
+	}
+	return doutput;
+}
+
+
+btn_generate.onclick = newwep;
+
+function newwep() {
+	//faction
+	var newfac=defobj.faction[dice(defobj.faction.length)];
+	var newclass=defobj.wepclass[dice(defobj.wepclass.length)];
+	var newtype=defobj.weptype[dice(defobj.weptype.length)];
+	var newsubtype=defobj.subtype[dice(defobj.subtype.length)];
+	var newenemy=defobj.faction[dice(defobj.faction.length)];
+	var wepname=classobj[newclass][newfac];
+	var weptype=typeobj[newtype][newsubtype];
+
+	var newattach=detailobj.attach[dice(detailobj.attach.length)];
+	var newdetail=detailobj.detail[dice(detailobj.detail.length)];
+	var newcons=detailobj.cons[dice(detailobj.cons.length)];
+	var newage=detailobj.age[dice(detailobj.age.length)];
+	//do description
+	var newdescnum=dice(descobj.length);
+	var paramArray = new Array();
+	paramArray=JSON.parse(JSON.stringify(descobj[newdescnum].params));
+	var descstring=descobj[newdescnum].descString;
+	for (var i = 0; i < paramArray.length; i++) {
+		if (paramArray[i]=="*solider*") {
+			paramArray[i]=factionobj.faction[newfac][dice(factionobj.faction[newfac].length)];
+		}
+		if (paramArray[i]=="*enemy*") {
+			paramArray[i]=factionobj.faction[newenemy][dice(factionobj.faction[newenemy].length)];
+		}
+		if (paramArray[i]=="*ammo*") {
+			paramArray[i]=weptype;
+		}
+		if (paramArray[i]=="*weapon*") {
+			console.log(wepname);
+			paramArray[i]=wepname;
+		}
+	}
+	var descs=dreplace(descstring,paramArray);
+
+
+	//end doing description
+
+	var finalwep="You have generated a ";
+	finalwep=finalwep.concat(newfac," ",wepname,". It ",newdetail," and a",newattach,". It fires ",weptype,"s.\n","Unfortunately it",newcons,". Its age is ",newage,"\n",descs,"\n ________\n");
+	ta_output.value=ta_output.value.concat(finalwep);
+}
